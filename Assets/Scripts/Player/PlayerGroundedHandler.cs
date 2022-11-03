d73964fb0aea837d1bc033ca3f32751be7877725
Scraxtastic <nick.u19990@gmail.com>
2022-11-03 21:55:26 +0100

    Grounded Script for all directions

--- Assets/Scripts/Player/PlayerGroundedHandler.cs
+++ Assets/Scripts/Player/PlayerGroundedHandler.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Unity.VisualScripting;
 
 public class PlayerGroundedHandler : MonoBehaviour
 {
+    enum Directions { Up, Down, Left, Right };
+    [SerializeField] private Directions direction = Directions.Down;
     [SerializeField] private string[] tagsForActivation = new string[] { "Map" };
     [SerializeField] public float maxHeight;
     [HideInInspector] public Rigidbody2D rigidbody;
@@ -16,9 +19,25 @@ public class PlayerGroundedHandler : MonoBehaviour
 
     void FixedUpdate()
     {
+        Vector2 dir = Vector2.up;
+        switch (direction)
+        {
+            case Directions.Up:
+                dir = Vector2.up;
+                break;
+            case Directions.Down:
+                dir = Vector2.down;
+                break;
+            case Directions.Left:
+                dir = Vector2.left;
+                break;
+            case Directions.Right:
+                dir = Vector2.right;
+                break;
+        }
         if (rigidbody == null) return;
         // Cast a ray straight down.
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir);
 
         // If it hits something...
         if (hit.collider != null)
